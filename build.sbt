f76b78cf33df17a3eb3616f50ce4bf0e1285f080
Gautam Roy <groy.pi@gmail.com>
2011-11-30 07:46:36 +0530

    all files commit for first time

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "scala-lab"
+
+version := "1.0"
+
+scalaVersion := "2.9.1"
+
+libraryDependencies += "org.scalaz" %% "scalaz-core" % "6.0.3"
---
+++ src/main/scala/Hi.scala
@@ -0,0 +1,7 @@
+object Hi { 
+  def main(args: Array[String]) {
+    println("Hi! Hello world with Scala + SBT + IDEA IntelliJ")
+
+    println(List(1, 2, 3).map(_.toString))
+  }
+}
